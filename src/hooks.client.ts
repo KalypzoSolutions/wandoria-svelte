4f6584021441954b8ecd87695050888cddea4f0f
EinJOJO <admin@einjojo.it>
2025-01-27 19:56:16 +0100

    added hotjar to html

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,10 +1,8 @@
 import { version } from '$lib/config';
-import Hotjar from '@hotjar/browser';
 import { H } from 'highlight.run';
 const siteId = 5267272;
 const hotjarVersion = 6;
 
-Hotjar.init(siteId, hotjarVersion);
 H.init('6gl5pyme', {
     environment: 'production',
     version: version,
