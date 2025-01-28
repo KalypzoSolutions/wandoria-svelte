b878f55b616734ed3cea7ffa9e2549f02410cfe2
EinJOJO <admin@einjojo.it>
2025-01-28 20:42:30 +0100

    Add community statement and server architecture blog pages with images

--- src/routes/News.svelte
+++ src/routes/News.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
+	import DiscordWidget from '$lib/DiscordWidget.svelte';
     import statement_thumb from '$lib/images/planet.avif';
+    import spaltung from '$lib/images/spaltung.avif';
 
     const newsArticles = [
         {
             title: "Community Statement | Gründe, Vorwürfe und die Zukunft von Wandoria",
             type: "Statement",
-            thumbnail: statement_thumb
+            thumbnail: spaltung,
+            href: "/blog/community-statement"
         },            
     {
             title: "Unsere Server Architektur",
             type: "TECH BLOG",
+            href: "/blog/server-architektur",
             thumbnail: statement_thumb
-        },
-        {
-            title: "Wie viel kostet die Entwicklung von Wandoria?",
-            type: "TECH BLOG",
-            thumbnail: statement_thumb
-        },
+        },        
         
     ];
 </script>
@@ -25,18 +24,20 @@
     <div class="text-zinc-200">
         <div class="flex justify-between items-center mb-8">
             <h1 class="text-4xl font-bold">Was passiert gerade?</h1>
+            <!--
             <button class="bg-zinc-800 px-4 py-2 rounded-md text-sm font-medium uppercase  hover:bg-zinc-700 transition-colors">Erfahre mehr</button>
+            -->
         </div>
 
         <div class="grid grid-cols-1 lg:grid-cols-[1fr,580px] gap-4">
             <!-- Primary News Card -->
             <div class="overflow-hidden ">
-                <a href="#news" class="block  group bg-zinc-900 hover:bg-zinc-800 transition-colors rounded-lg ">
-                    <div class="aspect-video w-full relative overflow-hidden">
+                <a href={newsArticles[0].href} class="block  group bg-zinc-900 hover:bg-zinc-800 transition-colors rounded-lg ">
+                    <div class="aspect-video w-full relative overflow-hidden rounded-lg">
                         <img 
                             src={newsArticles[0].thumbnail} 
                             alt={newsArticles[0].title}
-                            class="w-full h-full object-cover group-hover:scale-105 transition-all duration-300"
+                            class="w-full h-full  object-cover group-hover:scale-105 transition-all duration-300"
                         />
                     </div>
                     <div class="p-6">
@@ -52,7 +53,7 @@
             <div class="flex flex-col gap-4">
                 {#each newsArticles.slice(1) as article}
                     <div class="bg-zinc-900 rounded-lg overflow-hidden hover:bg-zinc-800 hover:-translate-y-1 transition-transform duration-500">
-                        <a href="#news" class="flex items-center gap-4 pl-4 p-2">
+                        <a href={article.href} class="flex items-center gap-4 pl-4 p-2">
                             <div class="flex-grow">
                                 <div class="flex items-center gap-2 mb-2">
                                     <span class="text-zinc-400 text-sm">{article.type}</span>
@@ -62,13 +63,13 @@
                             <img 
                                     src={article.thumbnail} 
                                     alt={article.title}
-                                    width={300}
-                                    height={(16 / 9) * 300}
-                                    class="w-[230px] aspect-video rounded-md"
+                                    width={230}
+                                    height={(16 / 9) * 230}
+                                    class="w-[160px] md:w-[230px] aspect-video rounded-md"
                                 />
                         </a>
                     </div>
-                {/each}
+                {/each}                
             </div>
         </div>
     </div>
---
+++ src/routes/blog/server-architektur/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import server_architektur from '$lib/images/blog/node.avif';
+</script>
+<small>
+    1 Minute | Veröffentlicht am 28.01.2025
+</small>
+<h1>Server Architektur</h1>
+<p>Wir teilen unsere Server Architektur, in der Hoffnung, dass es jemand besser weiß und uns hilft die Kosten niedrig zu halten!</p>
+<p> Unsere Serverarchitektur sieht mehrere Hetzer Cloud Server vor, die on-demand skaliert werden sollen. Jedenfalls während der Beta.</p>
+<div class="">
+    <img src={server_architektur} class="border border-zinc-800 mb-2" alt="" />
+<small>Das Bild enthält 3 Nodes, wovon eine Node (ext-mchost-1) für andere Projekte reserviert ist.</small>
+</div>
+<p>Das Wandoria Netzwerk besteht aus 2 Nodes.</p>
+<ul>
+    <li>1. Hetzner Cloud Ampere 4 Threads 8GB (Proxy & Database) 7€</li>
+    <li>2. Hetzner Cloud Ampere 8 Threads 16GB (Game Server) 14€</li>
+</ul>
+<p>
+    Für die Entwicklung ist dieses Konstrukt ausreichend.    
+    Mit den Servern <i>lobby, realms, farmwelt</i> sollte es möglich sein, die Beta zu starten.
+    Weil wir hoffen, dass nur wenige Spieler es schaffen alle Upgrades beim Raumschiff zu erreichen, werden sie in 
+</p>
+<p>
+    Sollten weitere Fragen zur Architektur bestehen, so können diese gerne im Discord gestellt werden. Zeitlich lohnt es sich aktuell so kurz vor der Beta nicht einen Roman hier zu veröffentlichen, denn es gibt noch andere Baustellen.
+</p>
+<hr />
+<small>Johannes | EinJOJO</small>
+
+
